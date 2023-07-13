cc2889e42d7119cd469a4b85434f19ad77577da9
Alan Nguyen <alannguyen@Alans-MacBook-Pro.local>
2023-07-13 14:57:14 -0700

    AASC added

--- src/Components/ProjectsHome/ProjectsHome.jsx
+++ src/Components/ProjectsHome/ProjectsHome.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ProjectsHome.css';
 import Cards from '../Cards/Cards';
+import AASC from '../../Assets/CardImages/aasc.png';
 import TCW from '../../Assets/CardImages/TCW.png';
 import DopaMind from '../../Assets/CardImages/dopamind.png';
 import JSB from '../../Assets/CardImages/jsb.png';
@@ -8,24 +9,28 @@ import MentorSEAS from '../../Assets/CardImages/mentorSEAS.png';
 
 function ProjectsHome() {
   const names = [
+    'AASC',
     'TransCanWork',
     'DopaMind',
     'SVM Classification',
     'MentorSEAS',
   ];
   const descriptions = [
+    'TEXTBOOK APPLICATION',
     'WEB APPLICATION',
     'HACKATHON PROJECT',
     'MACHINE LEARNING CLASSIFIER',
     'SORTING ALGORITHM',
   ];
   const images = [
+    AASC,
     TCW,
     DopaMind,
     JSB,
     MentorSEAS,
   ];
   const routes = [
+    '/wip',
     '/tcw',
     '/dopamind',
     '/svm',
--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ProjectsPage.css';
 import Header from '../Components/Header/Header';
 import ProjectPageCards from '../Components/ProjectPageCards/ProjectPageCards';
+import AASC from '../Assets/ProjectImages/aasc.png';
 import TCW from '../Assets/ProjectImages/TCW.png';
 import DopaMind from '../Assets/ProjectImages/DopaMind.png';
 import SVM from '../Assets/ProjectImages/SVM.png';
@@ -9,30 +10,35 @@ import MentorSEAS from '../Assets/ProjectImages/mentorSEAS.png';
 
 function ProjectsPage() {
   const projectNames = [
+    'UCLA Asian American Studies Center',
     'TransCanWork',
     'DopaMind',
     'SVM Classification',
     'MentorSEAS',
   ];
   const projectTypes = [
+    'TEXTBOOK APPLICATION',
     'WEB APPLICATION',
     'HACKATHON PROJECT',
     'MACHINE LEARNING CLASSIFIER',
     'SORTING ALGORITHM',
   ];
   const projectDescriptions = [
+    'An interactive textbook application deepening the public understanding of Asian American and Pacific Islander lives towards a healthy, just, democratic and compassionate society for all.',
     'An interactive employment roadmap for the 501(c)(3) nonprofit TransCanWork, aiding transgender, gender-nonconforming and intersex jobseekers in their job search.',
     'A comprehensive web app for students with ADHD, designed to help improve their focus and social skills by providing them with fun and engaging activities that are tailored to their unique needs.',
     'A Python program that employs a Support Vector Machine to classify mouse brain cells using single cell RNA-sequencing data.',
     'Utilizes the k-means algorithm to pair 1500 incoming engineering students with mentors, considering dozens of qualitative parameters to create axes for optimal pairings.',
   ];
   const projectImages = [
+    AASC,
     TCW,
     DopaMind,
     SVM,
     MentorSEAS,
   ];
   const projectRoutes = [
+    '/wip',
     '/tcw',
     '/dopamind',
     '/svm',
