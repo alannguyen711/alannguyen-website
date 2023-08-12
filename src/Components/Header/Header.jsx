0ee5edc1c5733d10771772d0a1456613008e84e9
Alan Nguyen <alannguyen@Alans-MacBook-Pro.local>
2023-08-12 00:47:01 -0700

    final touches, new logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-import alanLogo from '../../Assets/alan-logo.png';
+import alanLogo from '../../Assets/Logos/signature.png';
 import GreyArrow from '../../Assets/Icons/grey-arrow.svg';
 import Resume from '../../Assets/AlanNguyen_Resume.pdf';
 
